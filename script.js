68a0f681f636b713773ce6a2d08867cc98867936
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-10-25 12:45:26 -0400

    before adding directions part

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ var infoWindow;
 var service;
 var request;
 var markers = [];
+var directionsDisplay = new google.maps.DirectionsRenderer();
+var directionsService = new google.maps.DirectionsService();
+var htmls = [];
+var to_htmls = [];
+var from_htmls = [];
 
 function initialize(){
+  //creates initiali starting point
   var center = new google.maps.LatLng(37.4222, -122.084058);
   map = new google.maps.Map(document.getElementById('map'), {
     center: center,
     zoom: 12
   });
+  //finding cafes within 5miles of starting location
   request = {
     location: center,
     radius: 8047, //in meters. equals 5 miles
@@ -21,6 +28,8 @@ function initialize(){
 
   service.nearbySearch(request, callback);
 
+  //setting new center
+  //will clear previous markers and create new ones
   google.maps.event.addListener(map, 'dblclick', function(event){
     map.setCenter(event.latLng)
     clearResults(markers)
@@ -35,6 +44,8 @@ function initialize(){
   })
 }
 
+//pushing map markers into markers array
+//has to be less than 9, otherwise can't see address
 function callback(results, status){
   if (status == google.maps.places.PlacesServiceStatus.OK){
     for (var i = 0; i < 9; i++){
@@ -43,22 +54,25 @@ function callback(results, status){
   }
 }
 
+//creating map markers for each cafe found
 function createMarker(place){
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
+  //when you click on a map marker, will show name, address, and rating
   var request = { reference: place.reference };
   service.getDetails(request, function(details, status) {
     google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.setContent(details.name + '<br>' + details.formatted_address);
+      infoWindow.setContent('<strong>' + details.name + '</strong><br>' + details.formatted_address + '<br>' + details.rating + '/5 stars<br> Open Now: ' + details.opening_hours.open_now);
       infoWindow.open(map, this);
     });
   })
   return marker
 }
 
+//clears the previously made markers when you create a new center
 function clearResults(markers){
   for (var m in markers){
     markers[m].setMap(null)
